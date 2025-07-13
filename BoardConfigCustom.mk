90233e7575f4feefc7d5235300ee3f381f9fdb2a
RoshanGP4A <48382822+Roshan0746@users.noreply.github.com>
2025-07-13 12:28:37 +0530

    Update BoardConfigCustom.mk

--- BoardConfigCustom.mk
+++ BoardConfigCustom.mk
@@ -13,7 +13,7 @@ TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/sunfish/aosp_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/infinity/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aosp/config/device_framework_matrix.xml
 
 # Partitions
 AB_OTA_PARTITIONS += \
