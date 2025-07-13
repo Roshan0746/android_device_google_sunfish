348907c28e423305a4459c8a1432b4c80e87766b
Khaliq <bias.khaliq@gmail.com>
2025-07-13 10:39:46 +0700

    Update BoardConfigCustom.mk

--- BoardConfigCustom.mk
+++ BoardConfigCustom.mk
@@ -12,7 +12,7 @@ TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
 
 # Manifests
-DEVICE_MANIFEST_FILE += device/google/sunfish/lineage_manifest.xml
+DEVICE_MANIFEST_FILE += device/google/sunfish/aosp_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aosp/config/device_framework_matrix.xml
 
 # Partitions
